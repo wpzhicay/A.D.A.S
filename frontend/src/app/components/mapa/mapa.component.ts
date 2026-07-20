910a9b640a2dda1f3af34425a022a13189ac34f5
Tu Nombre <tu-email@example.com>
2026-07-19 20:55:57 -0500

    Integrate Leaflet map component with GPS tracking

--- frontend/src/app/components/mapa/mapa.component.ts
+++ frontend/src/app/components/mapa/mapa.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-mapa',
   templateUrl: './mapa.component.html',
+  styleUrls: ['./mapa.component.css'],
   standalone: true,
   imports: [CommonModule],
 })
-export class MapaComponent {}
+export class MapaComponent implements AfterViewInit {
+  map!: L.Map;
+  marker!: L.Marker;
+  latitud = -2.926469;
+  longitud = -78.951933;
+  velocidad = 0.31;
+
+  ngAfterViewInit() {
+    this.map = L.map('map').setView([this.latitud, this.longitud], 15);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+    }).addTo(this.map);
+
+    const icon = L.icon({
+      iconUrl:
+        'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png',
+      shadowUrl:
+        'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+    });
+
+    this.marker = L.marker([this.latitud, this.longitud], { icon })
+      .addTo(this.map)
+      .bindPopup(
+        `
+        <b>ESP32 Solar</b><br>
+        Voltaje: 12.5V<br>
+        Batería: 90%<br>
+        Velocidad: ${this.velocidad} km/h
+      `
+      )
+      .openPopup();
+  }
+
+  updateMarker(lat: number, lon: number) {
+    this.latitud = lat;
+    this.longitud = lon;
+    this.marker.setLatLng([lat, lon]);
+    this.map.setView([lat, lon], 15);
+  }
+}
